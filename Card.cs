9d44a47064a8b98ef9a1170b7391829f1365b18a
Léonid Shurygin <leonid.shurygin@u-psud.fr>
2022-01-05 17:52:53 +0100

    Add project files.

---
+++ Card.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SetGameSimulationByLeonid
+{
+    public class Card
+    {
+        public string Color { get; set; }
+        public char Symbol { get; set; }
+        public char Shading { get; set; }
+        public int Number { get; set; }
+
+        public Card() { }
+
+        public static Card GetCard(List<string> propertyString)
+        {
+            Card card = new Card();
+            card.SetColor(propertyString);
+            card.SetShadingAndSymbol(propertyString);
+            card.SetNumber(propertyString);
+
+            return card;
+        }
+
+        #region Construction of the Card model
+        private void SetColor(List<string> propertyString)
+        {
+            Color = propertyString[0].ToString();
+        }
+
+        private void SetNumber(List<string> propertyString)
+        {
+            Number = propertyString[1].ToString().Length;
+        }
+        private void SetShadingAndSymbol(List<string> propertyString)
+        {
+            string shadingWithNumbers = propertyString[1].Substring(0, 1);
+            Shading = shadingWithNumbers[0];
+            SetSymbol(propertyString);
+        }
+
+        private void SetSymbol(List<string> shading)
+        {
+            if (Shading == '\0' || Shading == ' ')
+            {
+                throw new Exception("Shading is void.");
+            }
+            else
+            {
+                if (Shading.Equals('a') || Shading.Equals('A') || Shading.Equals('@'))
+                {
+                    Symbol = 'A';
+                }
+                if (Shading.Equals('s') || Shading.Equals('S') || Shading.Equals('$'))
+                {
+                    Symbol = 'S';
+                }
+                if (Shading.Equals('h') || Shading.Equals('H') || Shading.Equals('#'))
+                {
+                    Symbol = 'H';
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SetGameSimulationByLeonid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 0;
+            string cardString;
+            List<Card> cardList = new List<Card>();
+
+            while (N < 3 || N > 30)
+            {
+                Console.WriteLine("Choose the number of cards : ");
+                N = Int16.Parse(Console.ReadLine());
+
+                if(N < 3 || N > 30)
+                {
+                    Console.WriteLine("The number N must be 3 and 30 included. Re-enter the number N please. \n");
+                }
+            }
+
+            Console.WriteLine("Enter your cards : \n");
+            while (N > 0)
+            {
+                cardString = Console.ReadLine();
+
+                var cardProperties = new List<string>(cardString.Split(' '));
+
+                Card card = Card.GetCard(cardProperties);
+                cardList.Add(card);
+
+                N -= 1;
+            }
+        }
+    }
+}
